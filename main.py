c65450922160889bb230229fe111c53b17ce587d
shun <1ntegrale9uation@gmail.com>
2024-05-03 20:22:09 +0900

    init

---
+++ main.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+from constants import TOKEN
+
+extensions = (
+    'timer',
+)
+
+
+class MyBot(commands.Bot):
+    def __init__(self):
+        super().__init__(
+            command_prefix=commands.when_mentioned_or('$ '),
+            help_command=None,
+            intents=discord.Intents.all(),
+        )
+
+    async def setup_hook(self):
+        for extension in extensions:
+            await self.load_extension(f'extensions.{extension}')
+        await self.tree.sync()
+
+
+def main():
+    MyBot().run(TOKEN)
+
+
+if __name__ == '__main__':
+    main()
